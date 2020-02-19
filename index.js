6b39f9e1c06c323a0db72b922f1936750f34866c
Jacqueline Cameron <jskye429@mail.com>
2020-02-18 21:59:32 -0600

    setting up for pull from windows

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const axios = require("axios");
+const pdf = require('pdfkit');
 const inquirer = require("inquirer");
+
 inquirer
 .prompt({
     message: "Enter your GitHub username",
@@ -24,3 +26,35 @@ inquirer
       })
     })
   });
+
+
+
+//   const fs = require("fs");
+// const axios = require("axios");
+// const inquirer = require("inquirer");
+
+// inquirer
+//   .prompt({
+//     message: "Enter your GitHub username:",
+//     name: "username"
+//   })
+//   .then(function({ username }) {
+//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//     axios.get(queryUrl).then(function(res) {
+//       const repoNames = res.data.map(function(repo) {
+//         return repo.name;
+//       });
+
+//       const repoNamesStr = repoNames.join("\n");
+
+//       fs.writeFile("repos.txt", repoNamesStr, function(err) {
+//         if (err) {
+//           throw err;
+//         }
+
+//         console.log(`Saved ${repoNames.length} repos`);
+//       });
+//     });
+//   });
+
