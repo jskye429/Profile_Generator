e416ecdb3dd248c5c2a29a1aa92b2ff9210bcc8a
Jacqueline Cameron <jskye429@mail.com>
2020-03-06 02:37:50 -0600

    still has unexpected token errors pushing for feedback

--- index.js
+++ index.js
@@ -2,59 +2,98 @@ const fs = require("fs");
 const axios = require("axios");
 const pdf = require('pdfkit');
 const inquirer = require("inquirer");
+const util = require("util");
 
 inquirer
 .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
+  
+    type: "input",
+    message: "What is your git username?",
+    name: "git"
+},
+{
+    type: "list",
+    message: "What is your favorite color?",
+    name: "color",
+    choices: colors
+})
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    
     axios.get(queryUrl)
     .then(function(res){
+      
       let arr = res.data.map(repo => {
+      
       return repo.name
       });
       //console.log(arr)
       let str = arr.join("\n")
       console.log(str)
-      fs.writeFile("log.txt", str, function(err){
+      
+      fs.writeFile("log.txt", "profile.pdf", str, function(err){
         if (err) {
         return console.log(err);
         }
-        console.log(arr.length + "repos saved")
-      })
-    })
-  });
+        else  {
+
+  const print = new pdf;
+      // pdf formating stuff
+    doc.rect(0,0,600,700)
+    .fillColor("grey")
+    .fillAndStroke(res.choices)
+  
+    doc.pipe(fs.createWriteStream("profile.pdf"));
+
+    doc.fontSize(24)
+    .fillColor("black")
+    .text("It's me" + res.data.name,{
+      width: 500,
+      align: "center"
+
+    });
+    
+
+
+doc.fontSize(10)
+.fillColor("black")
+.text("Find my face at" + JSON.stringify(results.data.avatar_url),{
+    align: "center"
+});
+
+doc.font("Times-Roman")
+.moveDown(5)
+.fontSize(18)
+.text("Followers" + JSON.stringify(results.data.followers),{
+    align: "left",
+    continued:true
+});
+
+doc.font("Times-Roman")
+.moveDown(3)
+.fontSize(18)
+.text("Location" + JSON.stringify(results.data.location),{
+    align: "left",
+    continued:true
+})
+.text("GitHub:" + JSON.stringify(results.data.data.html_url),{
+    align: "right"
+
+)};
 
 
 
-//   const fs = require("fs");
-// const axios = require("axios");
-// const inquirer = require("inquirer");
 
-// inquirer
-//   .prompt({
-//     message: "Enter your GitHub username:",
-//     name: "username"
-//   })
-//   .then(function({ username }) {
-//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+      )
 
-//     axios.get(queryUrl).then(function(res) {
-//       const repoNames = res.data.map(function(repo) {
-//         return repo.name;
-//       });
+print.end();                
+}                                         
+});         
+});
+});
 
-//       const repoNamesStr = repoNames.join("\n");
 
-//       fs.writeFile("repos.txt", repoNamesStr, function(err) {
-//         if (err) {
-//           throw err;
-//         }
 
-//         console.log(`Saved ${repoNames.length} repos`);
-//       });
-//     });
-//   });
 
+// doc.pipe(fs.createWriteStream('/path/to/file.pdf')); // write to PDF
+// doc.pipe(res);                                       // HTTP response
